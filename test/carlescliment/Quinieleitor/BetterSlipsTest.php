5968ac0994ccfa53958a60bab188ebf3abab4a9e
Carles Climent <carlescliment@hotmail.com>
2014-11-04 23:19:06 +0100

    Initial phpunit configuration

---
+++ test/carlescliment/Quinieleitor/BetterSlipsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace test\carlescliment\Quinieleitor;
+
+use carlescliment\Quinieleitor\BetterSlips;
+
+class BetterSlipsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itWorks()
+    {
+        $slip = new BetterSlips();
+    }
+    
+}
