c9a865ce4e5ba13269bc4a17bd42077a00cc1cd5
Carles Climent <carlescliment@hotmail.com>
2014-11-05 09:44:14 +0100

    Added connection as a service

--- config/drupal.php
+++ config/drupal.php
@@ -2,4 +2,10 @@
 
 $container = new Pimple\Container();
 
+$container['database.connection'] = function($c) {
+    $query_wrapper = new carlescliment\Components\DataBase\Adapter\Drupal\DBQueryWrapper();
+
+    return new carlescliment\Components\DataBase\Adapter\Drupal\Connection($query_wrapper);
+};
+
 return $container;
